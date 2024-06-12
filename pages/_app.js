bb2096357a6bd798989af8d3fff46707def4cf2c
laurisseau Joseph <reso0208@gmail.com>
2024-06-12 14:49:32 -0400

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-// pages/_app.js
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Gallery } from 'react-grid-gallery';
+import { Button, Nav } from 'react-bootstrap';
+
+// implement lazy loading and infinit scroll
 
 export async function getServerSideProps() {
   try {
     const response = await axios.get(
       'http://localhost:5000/api/user/getUserPhotos/66629208090d561a012f7669'
     );
-    const photos = response.data.photos;
+    const data = response.data;
     const serverTime = new Date().toString();
+    const photos = data.photos;
 
     return {
       props: {
@@ -26,18 +31,38 @@ export async function getServerSideProps() {
   }
 }
 
-const page = ({ serverTime, photos }) => {
+const Page = ({ photos: initialPhotos }) => {
+  const [photos, setPhotos] = useState(initialPhotos);
+
+  useEffect(() => {
+    //console.log('Photos updated:', photos); // these new photos that are selected will be sent back to admin
+  }, [photos]);
+
+  const handleSelect = (index, photo) => {
+    console.log(photo);
+    const newPhotos = [...photos];
+    newPhotos[index] = { ...photo, isSelected: !photo.isSelected };
+    setPhotos(newPhotos);
+  };
+
   return (
-    <div>
-      <div>
-        {photos?.map((photoUrl, index) => (
-          <div key={index} className="photo">
-            <img src={photoUrl} alt={`Photo ${index}`} loading="lazy" />
-          </div>
-        ))}
+    <>
+      {/* NavBar */}
+      <div className="container">
+        <Gallery
+          images={photos}
+          enableImageSelection={true}
+          onSelect={handleSelect}
+          margin={20}
+          rowHeight={280}
+        />
+
+        <div className="d-flex justify-content-center mb-5 mt-5">
+          <Button className="w-50 h-50">Submit</Button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default page;
+export default Page;
